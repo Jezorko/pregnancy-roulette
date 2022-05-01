a9584d80a3c6bdb2214580c4534f97af29c8bdc0
Paweł Szymański <pawelfiu@gmail.com>
2022-05-01 17:49:53 +0200

    Add subreddits to footer

--- src/jsMain/kotlin/components/Footer.kt
+++ src/jsMain/kotlin/components/Footer.kt
@@ -4,9 +4,11 @@ import api.ApiClient.getVersionInfo
 import jezorko.github.pregnancyroulette.VersionInfo
 import react.FC
 import react.Props
+import react.dom.html.ReactHTML
 import react.dom.html.ReactHTML.a
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.footer
+import react.dom.html.ReactHTML.span
 import react.useState
 
 val versionInfoPromise = getVersionInfo()
@@ -20,27 +22,49 @@ val Footer = FC<Props> { _ ->
         }
     }
 
-    val buildVersionInfoId = "build-version-info"
     val versionInfo = versionInfoState
 
     footer {
         id="footer"
 
+        val buildVersionInfoId = "build-version-info"
         if (versionInfo == null) {
-            div {
+            span {
                 +"Built from unknown commit"
                 id = buildVersionInfoId
+                className = "footer-element"
             }
         } else {
-            div {
+            span {
                 +"Built from "
                 id = buildVersionInfoId
+                className = "footer-element"
                 a {
                     id = "$buildVersionInfoId-link"
+                    className = "footer-element"
                     val commitUrl = "${versionInfo.repositoryUrl}/commit/${versionInfo.commitSlug}"
                     +versionInfo.commitSlug.substring(0, 7)
                     href = commitUrl
                 }
+            }
+        }
+
+        val inspiredById = "inspired-by"
+        span {
+            +"Inspired by"
+            id = inspiredById
+            className = "footer-element"
+            a {
+                id = "$inspiredById-childfree-link"
+                className = "footer-element"
+                +"r/childfree"
+                href = "https://www.reddit.com/r/childfree"
+            }
+            a {
+                id = "$inspiredById-antinatalism-link"
+                className = "footer-element"
+                +"r/antinatalism"
+                href = "https://www.reddit.com/r/antinatalism/"
             }
         }
     }
