a5088b73562974b0f917a491fa92bbde2d9dd9f5
Paweł Szymański <pawelfiu@gmail.com>
2022-05-08 12:48:37 +0200

    Add info about negative outcomes

--- src/jsMain/kotlin/components/OutcomeDetectedDescription.kt
+++ src/jsMain/kotlin/components/OutcomeDetectedDescription.kt
@@ -13,7 +13,7 @@ val OutcomeDetectedDescription = FC<OutcomeDetectedDescriptionProps> { props ->
     val numberOfOutcomes = props.numberOfNegativeOutcomes!!
     div {
         id = "outcomes-detected-description"
-        +"Ouchie, your pregnancy ended up with $numberOfOutcomes outcome${if (numberOfOutcomes > 1) "s" else ""}. We can always go for another one, eh?"
+        +"Ouchie, your pregnancy ended up with $numberOfOutcomes negative outcome${if (numberOfOutcomes > 1) "s" else ""}. We can always go for another one, eh?"
     }
 
 }
