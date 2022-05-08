7b08365989c05b9c2306ea4c980de7e945af193f
Paweł Szymański <pawelfiu@gmail.com>
2022-05-08 10:51:10 +0200

    Fix typo in disclaimer

--- src/jsMain/kotlin/components/WebsiteDisclaimer.kt
+++ src/jsMain/kotlin/components/WebsiteDisclaimer.kt
@@ -9,7 +9,7 @@ val WebsiteDisclaimer = FC<Props> { props ->
     div {
         id = "website-disclaimer"
         +"""DISCLAIMER: THIS WEBSITE DOES NOT PROVIDE MEDICAL ADVICE.
-                The information, including but not limited to, text, graphics, images and other material container on
+                The information, including but not limited to, text, graphics, images and other material contained on
                 this website are for informational purposes only. No material on this site is intended to be a substitute
                 for professional advice, diagnosis or treatment. Always seek the advice of your physician or other
                 qualified health care provider with any questions you may have regarding a medical condition or
