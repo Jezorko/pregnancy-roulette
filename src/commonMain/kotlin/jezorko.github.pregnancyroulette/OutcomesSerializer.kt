7ce47734463f55c0fcf18c7d8d8ecaff15fefb1b
Paweł Szymański <pawelfiu@gmail.com>
2022-05-15 08:14:14 +0200

    Flatten all outcomes

--- src/commonMain/kotlin/jezorko.github.pregnancyroulette/OutcomesSerializer.kt
+++ src/commonMain/kotlin/jezorko.github.pregnancyroulette/OutcomesSerializer.kt
@@ -5,6 +5,10 @@ interface Base64 {
     fun decode(input: String): String
 }
 
+private fun flattenOutcomes(outcomes: List<PregnancyOutcome>): List<PregnancyOutcome> = outcomes.flatMap { outcome ->
+    listOf(outcome) + flattenOutcomes(outcome.consecutiveOutcomes)
+}
+
 class OutcomesSerializer(private val base64: Base64, private val allOutcomes: List<PregnancyOutcome>) {
     fun serializeOutcomes(outcomes: List<PregnancyOutcome>) = outcomes.map { outcome ->
         allOutcomes.indexOfFirst { outcome.name == it.name }
@@ -13,7 +17,7 @@ class OutcomesSerializer(private val base64: Base64, private val allOutcomes: Li
     fun deserializeOutcomes(outcomes: String): List<PregnancyOutcome> = base64.decode(outcomes)
         .let { decodedOutcomes ->
             if (decodedOutcomes.isEmpty()) emptyList()
-            else if (decodedOutcomes == "EVERYTHING") allOutcomes
+            else if (decodedOutcomes == "EVERYTHING") flattenOutcomes(allOutcomes)
             else decodedOutcomes.split(",")
                 .map(String::toInt)
                 .map(allOutcomes::get)
